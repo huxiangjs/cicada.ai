ba6b4b3ac9932c61e6309676b60b8b5272a9aaa8
Hoozz <huxiangjs@foxmail.com>
2026-05-01 18:23:45 +0800

    更新天气工具
    Signed-off-by: Hoozz <huxiangjs@foxmail.com>

--- tools/function_weather/weather.py
+++ tools/function_weather/weather.py
@@ -6,7 +6,8 @@ def __function_call(arguments):
     """
     # ?format=3 参数可以返回一行精简的天气信息
     # url = f"https://wttr.in/{city_name}?format=3&lang=zh"
-    url = f"https://wttr.in/{arguments['city_name']}?format=地点:+%l\n+天气状况(表情符号):+%c\n+天气状况(文字):+%C\n+当前温度:+%t\n+体感温度:+%f\n+风速:+%w\n+湿度:+%h\n+降水量:+%p\n&lang=zh"
+    # url = f"https://wttr.in/{arguments['city_name']}?format=地点:+%l\n+天气状况(表情符号):+%c\n+天气状况(文字):+%C\n+当前温度:+%t\n+体感温度:+%f\n+风速:+%w\n+湿度:+%h\n+降水量:+%p\n&lang=zh"
+    url = f"https://wttr.in/{arguments['city_name']}?format=j2&lang=zh"
 
     try:
         response = requests.get(url, timeout=10)
@@ -23,7 +24,7 @@ function_desc_get_weather = {
         "type": "function",  # 工具类型，目前主要是 "function"
         "function": {
             "name": "get_weather",  # 函数名
-            "description": "获取指定城市的天气",  # 功能描述
+            "description": "获取指定城市当前的天气状况和未来2天的天气状况，返回为json数据",  # 功能描述
             "parameters": {  # 参数定义，遵循 JSON Schema
                 "type": "object",
                 "properties": {
